af6dae3e3a25013757c0cd330383a37765f58ed1
kupokomapa <kupokomapa@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-11-26 09:36:44 -0600

    [sfUtilitiesPlugin] Added page task for running batch scripts with better control over SF_* constants.
    git-svn-id: http://svn.symfony-project.com/plugins/sfUtilitiesPlugin/trunk@6169 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ data/tasks/sfCommandLineTask.php
@@ -0,0 +1,68 @@
+<?php
+
+pake_desc('Run a batch');
+pake_task('run', 'project_exists');
+
+function run_run($task, $args) 
+{
+  if (empty($args[0])) {
+    throw new sfException('Missing batch name');
+  }
+
+  if (!defined('SF_ROOT_DIR')) {
+    define('SF_ROOT_DIR', sfConfig::get('sf_root_dir'));
+  }  
+
+  $batchName = SF_ROOT_DIR.'/batch/'.$args[0].'.php';
+  
+  if (!file_exists($batchName)) {
+    throw new sfException("Can't find batch file : ".$batchName);
+  }
+
+  $argv = array();
+  for ($i = 1; $i < $_SERVER["argc"]; $i++) 
+  {
+    if ($_SERVER["argv"][$i]{0} === '-') 
+    {
+      // argument
+      $value =  (
+        isset($_SERVER["argv"][$i+1]) 
+        && 
+        $_SERVER["argv"][$i+1]{0} !== '-'
+        ?
+        $_SERVER["argv"][$i+1]
+        :
+        true
+      );
+      
+      if ($_SERVER["argv"][$i]{1} === '-') 
+      {
+        // long argument
+        $argv[substr($_SERVER["argv"][$i], 2)] = $value;
+      }
+      else 
+      {
+        foreach (str_split($_SERVER["argv"][$i]) as $arg) 
+        {
+          if (ereg('[a-zA-Z0-9]', $arg)) 
+          {
+            $last_arg   = $arg;
+            $argv[$arg] = true;
+          }
+        }
+        $argv[$last_arg] = $value;
+      }
+    }
+  }
+  
+  define('SF_APP',         (isset($argv['app'])?$argv['app']:'frontend'));
+  define('SF_ENVIRONMENT', (isset($argv['env'])?$argv['env']:'prod'));
+  define('SF_DEBUG',       (isset($argv['debug'])?1:0));
+
+  if (!is_dir(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP)) {
+    throw new sfException(sprintf('Appliction %s does not exist in the project', SF_APP));
+  }
+
+  require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+  require_once($batchName);
+}
